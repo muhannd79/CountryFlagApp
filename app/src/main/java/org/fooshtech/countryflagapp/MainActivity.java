7cf0e30e971adb5420ba8c965a168c17435637ae
muhannd79 <muhannddh@gmail.com>
2020-03-06 13:55:16 -0600

    Initial commit

---
+++ app/src/main/java/org/fooshtech/countryflagapp/MainActivity.java
@@ -0,0 +1,14 @@
+package org.fooshtech.countryflagapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
